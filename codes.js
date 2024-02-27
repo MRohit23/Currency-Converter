e2aee0e8697c5f717409166fcebb8e0d81621f8f
Rohit_R_Magar <magarrohit1953@gmail.com>
2024-02-27 22:54:15 +0530

    Intial commit

---
+++ codes.js
@@ -0,0 +1,12 @@
+const countryList = {
+    AED: "AE",
+    AUD: "AU",
+    EUR: "FR",
+    EGP: "EG",
+    CZK: "CZ",
+    INR: "IN",
+    JPY: "JP",
+    KRW: "KR",
+    RUB: "RU",
+    USD: "US",
+};
